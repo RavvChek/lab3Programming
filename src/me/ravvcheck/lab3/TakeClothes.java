171e74b86d0fbd5b069960879a2f518c8d979c96
RavvChek <119332654+RavvChek@users.noreply.github.com>
2022-12-23 08:46:08 +0300

    Delete TakeClothes.java

--- src/me/ravvcheck/lab3/TakeClothes.java
+++
@@ -1,9 +0,0 @@
-package me.ravvcheck.lab3;
-
-public interface TakeClothes {
-    default void notTakeOff(){
-        System.out.println("он не снимал одежду");
-    }
-
-    void takeOff(Thing obj);
-}
